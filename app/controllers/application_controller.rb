9d47124ad142697f1dc5782797321f0ef3392f28
Mike Kunkel <mike@mkunkel.com>
2016-06-16 14:22:45 -0500

    Log exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :null_session
+
+  rescue_from StandardError do |exception|
+    logger.error exception.message
+    exception.backtrace.each { |line| logger.error line }
+    raise exception
+  end
 end
