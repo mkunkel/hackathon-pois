6fc955ef8bdd2fe78196b989ff3ed3e4cb653347
Mike Kunkel <mike@mkunkel.com>
2016-06-16 15:28:46 -0500

    Allow all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :pois, only: [:create, :show, :index]
-  resources :tips, only: [:create, :show, :index]
-  resources :images, only: [:create, :show, :index]
+  resources :pois
+  resources :tips
+  resources :images
 end
